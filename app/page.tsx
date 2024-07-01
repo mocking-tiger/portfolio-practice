fbe97cccf2ee05f78f08d635487d0d10b6be2cde
Park JunYong <dyden119@gmail.com>
2024-07-01 23:02:48 +0900

    chore: projects섹션 띄어쓰기 교정

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Typewriter from "@/components/Typewriter";
 
 export default function Home() {
   return (
-    <main className="select-none relative overflow-hidden">
+    <main className="relative overflow-hidden">
       <NavContents />
       <header className="w-screen h-[200px] md:h-[300px] bg-home bg-center bg-cover bg-no-repeat flex justify-center items-end">
         <Typewriter />
