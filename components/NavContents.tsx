5c2899c3885eba79ade871d24a943f981fae21ff
Park JunYong <dyden119@gmail.com>
2024-07-05 23:02:15 +0900

    chore: 이미지 클릭으로 한/일 언어 변경

--- components/NavContents.tsx
+++ components/NavContents.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { useState } from "react";
+import { useLanguageStore } from "@/zustand/store";
 import Image from "next/image";
 import NavButton from "./NavButton";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function NavContents() {
   const [isClicked, setIsClicked] = useState(false);
   const [boxPosition, setBoxPosition] = useState(0);
   const [opacity, setOpacity] = useState("opacity-0");
+  const lang = useLanguageStore((state) => state.language);
+  const setLang = useLanguageStore((state) => state.setLanguage);
   const xValue = [
     "right-[332px]",
     "right-[246px]",
@@ -18,9 +21,9 @@ export default function NavContents() {
 
   return (
     <>
-      <nav className="min-w-[240px] h-fit py-20 hidden flex-col text-black bg-white fixed left-10 top-[20%] rounded-lg border z-50 2xl:flex">
+      <nav className="min-w-[200px] h-fit pt-20 pb-10 hidden flex-col text-black bg-white fixed left-10 top-[20%] rounded-lg border z-50 2xl:flex">
         <Image
-          className="relative left-[80px] -top-10"
+          className="relative left-[65px] -top-10"
           src="/buddha.png"
           width={60}
           height={60}
@@ -28,8 +31,8 @@ export default function NavContents() {
           onClick={() => setIsClicked((prev) => !prev)}
         />
         {isClicked && (
-          <div className="w-full p-3 text-center text-white rounded-3xl absolute -top-10 bg-[#27BCF3] after:absolute after:content-[''] after:border-[#27BCF3] after:border-t-[30px] after:top-[40px] after:right-[105px] after:border-l-[20px] after:border-r-[20px] after:border-l-transparent after:border-r-transparent transition-all duration-700">
-            항상 행복하세요
+          <div className="w-full p-3 text-center text-white rounded-3xl absolute -top-10 bg-[#27BCF3] after:absolute after:content-[''] after:border-[#27BCF3] after:border-t-[30px] after:top-[40px] after:right-[82px] after:border-l-[20px] after:border-r-[20px] after:border-l-transparent after:border-r-transparent transition-all duration-700">
+            {lang === "korean" ? "항상 행복하세요" : "いつもお幸せに"}
           </div>
         )}
         <div className="flex flex-col gap-12 overflow-hidden">
@@ -46,6 +49,24 @@ export default function NavContents() {
             <NavButton>Projects</NavButton>
           </Link>
         </div>
+        <div className="mt-10 flex justify-center">
+          <Image
+            className="cursor-pointer"
+            src="/korean.png"
+            width={60}
+            height={60}
+            alt="태극기 아이콘"
+            onClick={() => setLang("korean")}
+          />
+          <Image
+            className="cursor-pointer"
+            src="/japanese.png"
+            width={60}
+            height={60}
+            alt="일장기 아이콘"
+            onClick={() => setLang("japanese")}
+          />
+        </div>
       </nav>
       <nav className="w-full h-10 fixed 2xl:hidden bg-white shadow-xl z-50">
         <Image
@@ -56,7 +77,7 @@ export default function NavContents() {
           alt="부처 아이콘"
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
         />
-        <div className="w-full h-full pr-5 sm:pr-20 flex justify-end items-center gap-1 sm:gap-5 relative">
+        <div className="w-full h-full pr-16 sm:pr-20 flex justify-end items-center gap-1 sm:gap-5 relative">
           <div
             className={`w-[72px] h-[90%] absolute ${xValue[boxPosition]} ${opacity} hidden sm:block bg-black z-[-1] transition-all duration-150 rounded-md`}
           ></div>
@@ -128,6 +149,18 @@ export default function NavContents() {
               <span className="p-3 sm:hidden">P</span>
             </div>
           </Link>
+        </div>
+        <div>
+          <Image
+            className="absolute right-5 top-1 cursor-pointer"
+            src={lang === "korean" ? "/korean.png" : "/japanese.png"}
+            width={40}
+            height={40}
+            alt="현재 언어 아이콘"
+            onClick={() => {
+              lang === "korean" ? setLang("japanese") : setLang("korean");
+            }}
+          />
         </div>
       </nav>
     </>
--- components/Typewriter.tsx
+++ components/Typewriter.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { useLanguageStore } from "@/zustand/store";
 import useInterval from "@/hooks/useInterval";
-import { useState } from "react";
 
 export default function Typewriter() {
-  const text = "안녕하세요. 어제도 개발한 예비 개발자, 박준용입니다.";
+  const lang = useLanguageStore((state) => state.language);
+  const text =
+    lang === "korean"
+      ? "안녕하세요. 어제도 개발한 예비 개발자, 박준용입니다."
+      : "こんにちは。昨日も開発を致した、パク・ジュニョンと申します。";
   const [landingText, setLandingText] = useState("");
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setLandingText("");
+    setCount(0);
+  }, [lang]);
 
   useInterval(() => {
     if (count >= text.length) {
@@ -21,8 +31,9 @@ export default function Typewriter() {
   }, 100);
 
   return (
-    <p className="mb-10 pr-2 text-gray-300 font-pretendard text-center text-md md:text-4xl animate-typingCursor">
+    <p className="mb-10 pr-2 text-gray-300 font-pretendard text-center text-md md:text-4xl">
       {landingText}
+      <span className="animate-typingCursor"> </span>
     </p>
   );
 }
